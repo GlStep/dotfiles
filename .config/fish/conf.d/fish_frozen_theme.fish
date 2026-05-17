35982e59571d16d1ebd1e6e5f1a75695980cc4ec
Gleb Stepanov <gleb.stepanov@online.de>
2026-05-18 01:17:44 +0200

    Updated the fish config a bit, made it fresh

---
+++ .config/fish/conf.d/fish_frozen_theme.fish
@@ -0,0 +1,45 @@
+# Don't edit this file, as it will be written by the web-config tool (`fish_config`).
+# To customize your theme, delete this file and see
+#     help interactive#syntax-highlighting
+# or
+#     man fish-interactive | less +/^SYNTAX.HIGHLIGHTING
+# for appropriate commands to add to ~/.config/fish/config.fish instead.
+
+set --global fish_color_autosuggestion 555
+set --global fish_color_cancel --reverse
+set --global fish_color_command 4581eb
+set --global fish_color_comment fa5355
+set --global fish_color_cwd 126e00
+set --global fish_color_cwd_root fa5355
+set --global fish_color_end 126e00
+set --global fish_color_error fb7172
+set --global fish_color_escape 60d3d1
+set --global fish_color_history_current --bold
+set --global fish_color_host normal
+set --global fish_color_host_remote c2c300
+set --global fish_color_keyword 4581eb
+set --global fish_color_match --background=6d9df1
+set --global fish_color_normal normal
+set --global fish_color_operator 60d3d1
+set --global fish_color_option 33c2c1
+set --global fish_color_param 33c2c1
+set --global fish_color_quote c2c300
+set --global fish_color_redirection 33c2c1 --bold
+set --global fish_color_search_match ffff00 --background=555555
+set --global fish_color_selection adadad --bold --background=555555
+set --global fish_color_status fa5355
+set --global fish_color_user 67ff4f
+set --global fish_color_valid_path --underline=single
+set --global fish_pager_color_background 
+set --global fish_pager_color_completion normal
+set --global fish_pager_color_description ffff00 --italics
+set --global fish_pager_color_prefix normal --bold --underline=single
+set --global fish_pager_color_progress eeeeee --background=33c2c1
+set --global fish_pager_color_secondary_background 
+set --global fish_pager_color_secondary_completion 
+set --global fish_pager_color_secondary_description 
+set --global fish_pager_color_secondary_prefix 
+set --global fish_pager_color_selected_background --reverse
+set --global fish_pager_color_selected_completion 
+set --global fish_pager_color_selected_description 
+set --global fish_pager_color_selected_prefix 
---
+++ .config/fish/config.fish
@@ -0,0 +1,20 @@
+set -gx PYENV_ROOT $HOME/.pyenv
+fish_add_path $PYENV_ROOT/bin
+
+set -gx PNPM_HOME $HOME/Library/pnpm
+fish_add_path $PNPM_HOME
+
+fish_add_path $HOME/.local/bin
+
+fish_add_path $HOME/.lmstudio/bin
+
+fish_add_path $HOME/.cargo/bin
+
+fish_add_path /opt/homebrew/opt/ruby/bin
+
+if status is-interactive
+# Commands to run in interactive sessions can go here
+	pyenv init - fish | source
+end
+
+eval (/opt/homebrew/bin/brew shellenv)
